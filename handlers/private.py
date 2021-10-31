fbb22655fe12e3ef4136068ab5895bc4c22c3278
creatorPavan <93319387+creatorPavan@users.noreply.github.com>
2021-10-31 18:23:25 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,31 +9,36 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     
     await message.reply_text(
-        f"""**
-🌠𝗧𝗵𝗶𝘀 𝗜𝘀 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 \n🌺𝗥𝘂𝗻 𝗢𝗻 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗩𝗣𝗦 𝗦𝗲𝗿𝘃𝗲𝗿 \n🌼𝗙𝗲𝗲𝗹 𝗛𝗶𝗴𝗵 𝗤𝘂𝗮𝗹𝗶𝘁𝘆 𝗠𝘂𝘀𝗶𝗰 𝗜𝗻 𝗩𝗖 \n⭐𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 [𝗛𝗲𝘅𝗼𝗿](https://t.me/Its_Hexor)**
+        f"""**🏷️ Hᴇʏ, ɪ ᴀᴍ [ᴘᴀᴠᴀɴ'ᴢ ᴘʟᴀʏᴇʀ](https://telegra.ph/file/89d4135199d1d2a98596e.jpg).
+ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ ᴘʀᴇᴍɪᴜᴍ sᴜᴘᴇʀ
+ғᴀsᴛ ɴᴇxᴛ ɢᴇɴᴇʀᴀᴛɪᴏɴ ᴍᴜsɪᴄ
+ᴘʟᴀʏᴇʀ ʙᴏᴛ..!
+┏━━━━━━━━━━━━━━━━━┓
+┣★ ʀᴜɴ ᴏɴ ᴘʀɪᴠᴀᴛᴇ ᴠᴘꜱ ꜱᴇʀ. 
+┣★ ʜɪɢʜ ǫᴜᴀʟɪᴛʏ ᴍᴜꜱɪᴄ.
+┣★ ɴᴏ ʟᴀɢ ɪɴ ɪɴ ᴠᴏɪᴄᴇ.
+┣★ ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ.
+┗━━━━━━━━━━━━━━━━━┛
+ ᴄʀᴇᴀᴛᴇᴅ ʙʏ : [ᴄʀᴇᴀᴛᴏʀ ᴘᴀᴠᴀɴ](https://t.me/Pavanmagar)**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "❰𝗢𝘄𝗻𝗲𝗿❱", url="https://t.me/Sanki_Owner")
-                  ],[
-                    InlineKeyboardButton(
-                        "❰𝗦𝘂𝗽𝗽𝗼𝗿𝘁❱", url="https://t.me/EsportClan"
+                        "💽 𝗥𝗘𝗣𝗢", url="https://t.me/CreatorPavanRepo"
                     ),
                     InlineKeyboardButton(
-                        "❰𝗚𝗿𝗼𝘂𝗽❱", url="https://t.me/Prayagraj_Op"
-                    )
+                        "𝗕𝗢𝗦𝗦 😎", url="https://t.me/Pavanmagar")
                 ],[ 
                     InlineKeyboardButton(
-                        "❰𝗦𝗼𝘂𝗿𝗰𝗲❱", url="https://github.com/HEXOROP/eSportMusicX"
+                        "😀 𝗔𝗗𝗗 𝗠𝗘 😀", url="https://t.me/CrepanRobot?startgroup=true"
                     )]
             ]
         ),
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command("hexor") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("reload") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
       await message.reply_text("""**𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗢𝗻𝗹𝗶𝗻𝗲 𝗡𝗼𝘄\n🌠𝗛𝗲𝘅𝗼𝗿 𝗫𝗗 <3**""",
       reply_markup=InlineKeyboardMarkup(
